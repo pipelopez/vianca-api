c52639e862d3b10eafc3e94f025e64b2ab3f73b2
Carlos Jiménez <cjimenez@melollevo.com>
2017-05-05 22:37:59 -0500

    Starts application with a hello world

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "net/http"
+  "os"
+  "gopkg.in/gin-gonic/gin.v1"
+)
+
+func main() {
+  port := os.Getenv("PORT")
+  router := gin.Default()
+
+  if port == "" {
+    port = "8080"
+  }
+
+  router.GET("/", func(c *gin.Context) {
+    c.String(http.StatusOK, "Hola mundo")
+  })
+
+  router.Run(":" + port)
+
+}
