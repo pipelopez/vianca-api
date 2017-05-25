d0cfd9085b843b7d3db820466cf22dc2ef1e755e
dhmenesesl <dhmenesesl@gmail.com>
2017-05-24 21:04:51 -0500

    Se agregó función CORS y se solucionaron problemas con fechas

--- main.go
+++ main.go
@@ -9,18 +9,19 @@ import (
 	"gopkg.in/mgo.v2"
 	"fmt"
 	"time"
+	"strings"
 )
 
 type Flight struct {
-  Id bson.ObjectId `json:"id" bson:"_id"`
-  FlightCode string `json:"flightCode" bson:"flightCode"`
-  Origin string `json:"origin" bson:"origin"`
-  Destination string `json:"destination" bson:"destination"`
-  Price float32 `json:"price" bson:"price"`
-  Currency string `json:"currency" bson:"currency"`
-  Date time.Time `json:"date" bson:"date"`
-  Passengers int `json:"passengers" bson:"passengers"`
-  Capacity int `json:"capacity" bson:"capacity"`
+  //Id bson.ObjectId `json:"id" bson:"_id"`
+  FlightCode string //`json:"flightCode" bson:"flightCode"`
+  Origin string //`json:"origin" bson:"origin"`
+  Destination string //`json:"destination" bson:"destination"`
+  Price int //`json:"price" bson:"price"`
+  Currency string //`json:"currency" bson:"currency"`
+  Date time.Time //`json:"date" bson:"date"`
+  Passengers int //`json:"passengers" bson:"passengers"`
+  Capacity int //`json:"capacity" bson:"capacity"`
 }
 
 type FlightQuery struct {
@@ -31,13 +32,12 @@ type FlightQuery struct {
 	Passengers int
 	RoundTrip bool
 }
-
-const
 const (
 	code = 12345
 	name = "Vianca Airlines"
 	thumbnail = "https://image.freepik.com/icones-gratuites/avion-noir_318-31722.jpg"
 	layoutDate = "2006-01-02"
+
 )
 
 func main() {
@@ -58,7 +58,23 @@ func main() {
 	flightConection := session.DB("vianca-db").C("vuelo")
 	var results []Flight
 
+
+	router.POST("/inserts", func(c *gin.Context) {
+
+		CORS(c)
+		var json Flight
+		c.BindJSON(&json)
+
+		err = flightConection.Insert(&Flight{json.FlightCode, json.Origin, json.Destination, json.Price, json.Currency, json.Date, json.Passengers, json.Capacity})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
+
 	router.POST("/reserve", func(c *gin.Context) {
+
+		CORS(c)
 		var json Flight
 		c.BindJSON(&json)
 		fmt.Println(json)
@@ -67,16 +83,14 @@ func main() {
 	})
 
 	router.POST("/search", func(c *gin.Context) {
-		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
-		c.Next()
-		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "DELETE, POST, PUT")
 
+		CORS(c)
 		var query FlightQuery
+
 		c.BindJSON(&query)
 		fmt.Println(query)
-		err = flightConection.Find(bson.M{"origin": query.Origin, "destination": query.Destination, "passengers": query.Passengers, "date": {$lt: parseDate(query.DepartureDate)}).All(&results)
-
+		err = flightConection.Find(bson.M{"passengers": bson.M{"$gte": query.Passengers}, "origin": query.Origin, "destination": query.Destination, "date": bson.M{"$gt": parseDate(query.DepartureDate)}}).All(&results)
+		fmt.Println(parseDate(query.DepartureDate))
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -88,10 +102,19 @@ func main() {
 
 }
 
-func parseDate(date string) Time {
+
+func parseDate(date string) time.Time {
 	dateArray := strings.Split(date, "-")
-  inverseDateArray := []string{dateArray[2], dateArray[1], dateArray[0]}
-  correctDate := strings.Join(inverseDateArray, "-")
-  t, _ := time.Parse("2006-01-02", correctDate)
-  return t
+  	inverseDateArray := []string{dateArray[2], dateArray[1], dateArray[0]}
+  	correctDate := strings.Join(inverseDateArray, "-")
+  	t, _ := time.Parse("2006-01-02", correctDate)
+  	return t
+}
+
+func CORS(c *gin.Context){
+
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "DELETE, POST, PUT")
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		c.Next()
+
 }
